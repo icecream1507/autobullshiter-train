333775b0be3cee57bec8035fe46fd977a78b2d19
Ted Fu <ic@f-y.me>
2020-09-06 15:39:48 +0800

    采用四元高阶模型

--- generate.py
+++ generate.py
@@ -2,15 +2,15 @@ import redis
 import random
 from collections import Counter
 
-cli = redis.StrictRedis(host="localhost", port=6379, decode_responses=True)
-pre = ['', '']
-length = 0
+cli = redis.StrictRedis(host="localhost", port=6379, decode_responses=True, db=0)
+pre = ['', '', '']
+length = 200
 i = 0
 while i < length:
     print(pre[0], end='')
-    surfixes = cli.lrange("('%s', '%s')" % (pre[0], pre[1]), 0, -1)
-    word_counts = Counter(surfixes)
-    pre[0] = pre[1]
-    choices = word_counts.most_common(2)
-    pre[1] = random.choice(choices)[0]
+    t = pre
+    surfixes = cli.lrange("('%s', '%s', '%s')" % (pre[0], pre[1], pre[2]), 0, -1)
+    pre[0] = t[1]
+    pre[1] = t[2]
+    pre[2] = random.choice(surfixes)
     i += 1
--- train.py
+++ train.py
@@ -1,32 +1,39 @@
 import jieba
 import json
+import os
 import nltk
 import redis
 import time
 
 src = ''
 
-with open(src, 'r', encoding='UTF-8') as f:
-    text = f.read()
+def train(fn):
+    with open(fn, 'r', encoding='UTF-8') as f:
+        text = f.read().replace('\n', '')
 
-time_start = time.time()
-words = jieba.lcut(text)
-#bigrams = nltk.bigrams(words)
-trgrams = nltk.ngrams(words, 3)
-time_end = time.time()
-print('三元组分组完成，开始配置数据库。用时：', str(time_end-time_start))
+    time_start = time.time()
+    words = jieba.lcut(text)
+    four_grams = nltk.ngrams(words, 4)
+    time_end = time.time()
+    print('四元组分组完成，开始配置数据库。用时：', str(time_end-time_start))
 
 
-time_start = time.time()
-conn = redis.StrictRedis(host="localhost", port=6379, decode_responses=True)
-print('数据库配置完成，开始训练模型。用时：', str(time_end-time_start))
+    time_start = time.time()
+    conn = redis.StrictRedis(host="localhost", port=6379, decode_responses=True, db=0)
+    print('数据库配置完成，开始训练模型。用时：', str(time_end-time_start))
 
-time_start = time.time()
-for t in trgrams:
-    pre = "('%s', '%s')" % (t[0], t[1])
-    conn.rpush(pre, t[2])
-    print(conn.lrange(pre, 0, -1))
-time_end = time.time()
-print('模型训练完成。用时：', str(time_end-time_start))
+    time_start = time.time()
+    for i in four_grams:
+        pre = "('%s', '%s', '%s')" % (i[0], i[1], i[2])
+        conn.rpush(pre, i[3])
+    time_end = time.time()
+    print('模型训练完成。用时：', str(time_end-time_start))
 
-print("完成")
+os.chdir(src)
+files = os.listdir(src)
+total = len(files)
+for i in range(total):
+    fn = 'wiki_' + str(i)
+    print('正在训练：', fn)
+    train(fn)
+    print('该文件训练完成，进度：{:.2%}'.format((i+1)/total))
